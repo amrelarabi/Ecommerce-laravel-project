d65e130fadbdbfcd6f95734ef6c8964de6aae00c
amrelarabi <amr.elarabi.egy@gmail.com>
2019-04-26 19:40:25 +0200

    update logo

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -35,93 +35,88 @@
 <header class="header" id="site-header">
 
     <div class="container">
-
-        <div class="header-content-wrapper">
-            <form action="{{ route('product.search') }}" method="POST" role="search" id="search">
-                {{ csrf_field() }}
-                <div class="input-group">
-                    <input type="text" class="form-control" id="search_input" name="q" placeholder="Search products"> <span class="input-group-btn">
-                    <button type="submit" class="btn btn-default btn-search">
-                        <i class="fa fa-search"></i>
-                    </button>
-                    </span>
+        <div class="row">
+            <div class="header-content-wrapper">
+                <div id="logo">
+                    <h4 class="h1 heading-title">Ecommerce Project</h4>
                 </div>
-            </form>
-            <ul class="nav-add">
-                @if (Auth::check())
-                    <li class="login">
-                        <span>Hello {{ Auth::user()->name }}</span>
-                        <a href="{{ url('/logout') }}" onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">Logout <i class="fa fa-sign-out home-icon"></i></a>
-                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-                @else
-                    <li class="login">
-                        <a href="{{route('user.login')}}">Login <i class="fa fa-sign-in home-icon"></i></a>
-                    </li>
-                @endif
-                <li class="cart">
-
-                    <a href="#" class="js-cart-animate">
-                        <i class="seoicon-basket"></i>
-                        <span class="cart-count">{{ Cart::content()->count() }}</span>
-                    </a>
-
-                    <div class="cart-popup-wrap">
-                        <div class="popup-cart">
-                            <h4 class="title-cart align-center">${{ Cart::total() }}</h4>
-                            <br>
-                            <a href="/cart">
-                                <div class="btn btn-small btn--dark">
-                                    <span class="text">View cart</span>
-                                </div>
-                            </a>
-                        </div>
+                <form action="{{ route('product.search') }}" method="POST" role="search" id="search">
+                    {{ csrf_field() }}
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="search_input" name="q" placeholder="Search products"> <span class="input-group-btn">
+                        <button type="submit" class="btn btn-default btn-search">
+                            <i class="fa fa-search"></i>
+                        </button>
+                        </span>
                     </div>
+                </form>
+                <ul class="nav-add">
+                    @if (Auth::check())
+                        <li class="login">
+                            <span>Hello {{ Auth::user()->name }}</span>
+                            <a href="{{ url('/logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">Logout <i class="fa fa-sign-out home-icon"></i></a>
+                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    @else
+                        <li class="login">
+                            <a href="{{route('user.login')}}">Login <i class="fa fa-sign-in home-icon"></i></a>
+                        </li>
+                    @endif
+                    <li class="cart">
 
-                </li>
-               
-            </ul>
-            
+                        <a href="#" class="js-cart-animate">
+                            <i class="seoicon-basket"></i>
+                            <span class="cart-count">{{ Cart::content()->count() }}</span>
+                        </a>
+
+                        <div class="cart-popup-wrap">
+                            <div class="popup-cart">
+                                <h4 class="title-cart align-center">${{ Cart::total() }}</h4>
+                                <br>
+                                <a href="/cart">
+                                    <div class="btn btn-small btn--dark">
+                                        <span class="text">View cart</span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                    </li>
+                
+                </ul>
+                
+            </div>
         </div>
-       
     </div>
 
 </header>
 <div id="category_menu">
     <div class="container">
-        <nav class="nav">
-            @if(Request::route()->getName() == 'index')
-                <a class="nav-link active" href="/">All Categories</a>
-            @else
-                <a class="nav-link" href="/">All Categories</a>
-            @endif
-            @foreach($categories as $category)
-                @if(request()->id ==  $category->id)
-                    <a class="nav-link active" href="{{route('product.category', $category->id)}}">{{ $category->name }}</a>
+        <div class="row">
+            <nav class="nav">
+                @if(Request::route()->getName() == 'index')
+                    <a class="nav-link active all-categories" href="/">All Categories</a>
                 @else
-                    <a class="nav-link" href="{{route('product.category', $category->id)}}">{{ $category->name }}</a>
+                    <a class="nav-link all-categories" href="/">All Categories</a>
                 @endif
-            @endforeach
-        </nav>
+                @foreach($categories as $category)
+                    @if(request()->id ==  $category->id)
+                        <a class="nav-link active" href="{{route('product.category', $category->id)}}">{{ $category->name }}</a>
+                    @else
+                        <a class="nav-link" href="{{route('product.category', $category->id)}}">{{ $category->name }}</a>
+                    @endif
+                @endforeach
+            </nav>
+        </div>
     </div>
 </div>
 
 <div class="content-wrapper">
 
-    <div class="container">
-        <div class="row pt120">
-            <div class="col-lg-8 col-lg-offset-2">
-                <div class="heading align-center mb60">
-                    <h4 class="h1 heading-title">Ecommerce Project</h4>
-                    <p class="heading-text">Easy shopping, Easy Life.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <br>
 
     <!-- End Books products grid -->
 
